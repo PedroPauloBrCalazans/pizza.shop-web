96ba6b05ce53bf4eb39eea50738516f25bab1e4d
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2024-06-30 21:21:30 -0300

    feat: implementação do layout login..

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,25 +5,25 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "tailwind.config.cssinline-flex tailwind.config.cssitems-center tailwind.config.cssjustify-center tailwind.config.csswhitespace-nowrap tailwind.config.cssrounded-md tailwind.config.csstext-sm tailwind.config.cssfont-medium tailwind.config.cssring-offset-background tailwind.config.csstransition-colors focus-visible:tailwind.config.cssoutline-none focus-visible:tailwind.config.cssring-2 focus-visible:tailwind.config.cssring-ring focus-visible:tailwind.config.cssring-offset-2 disabled:tailwind.config.csspointer-events-none disabled:tailwind.config.cssopacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "tailwind.config.cssbg-primary tailwind.config.csstext-primary-foreground hover:tailwind.config.cssbg-primary/90",
+        default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
-          "tailwind.config.cssbg-destructive tailwind.config.csstext-destructive-foreground hover:tailwind.config.cssbg-destructive/90",
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "tailwind.config.cssborder tailwind.config.cssborder-input tailwind.config.cssbg-background hover:tailwind.config.cssbg-accent hover:tailwind.config.csstext-accent-foreground",
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "tailwind.config.cssbg-secondary tailwind.config.csstext-secondary-foreground hover:tailwind.config.cssbg-secondary/80",
-        ghost: "hover:tailwind.config.cssbg-accent hover:tailwind.config.csstext-accent-foreground",
-        link: "tailwind.config.csstext-primary tailwind.config.cssunderline-offset-4 hover:tailwind.config.cssunderline",
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "tailwind.config.cssh-10 tailwind.config.csspx-4 tailwind.config.csspy-2",
-        sm: "tailwind.config.cssh-9 tailwind.config.cssrounded-md tailwind.config.csspx-3",
-        lg: "tailwind.config.cssh-11 tailwind.config.cssrounded-md tailwind.config.csspx-8",
-        icon: "tailwind.config.cssh-10 tailwind.config.cssw-10",
+        default: "h-10 px-4 py-2",
+        sm: "h-9 rounded-md px-3",
+        lg: "h-11 rounded-md px-8",
+        icon: "h-10 w-10",
       },
     },
     defaultVariants: {
