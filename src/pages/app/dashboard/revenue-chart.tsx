525eecaa99d4e5aea3dfdb84dc6e3e73ce3b5a15
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2024-07-19 22:35:54 -0300

    feat: Iniciando componente de gráfico.

---
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,37 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import {
+  ResponsiveContainer,
+  LineChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Line,
+  Tooltip,
+} from "recharts";
+
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no período
+          </CardTitle>
+          <CardDescription>Receita diária no período</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart style={{ fontSize: 12 }}></LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  );
+}
