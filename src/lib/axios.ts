f5ac166f04f1505f3a9ac24b309a7c842591b541
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2024-08-13 16:55:37 -0300

    feat: Implementação de loading no perfil.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,3 +5,11 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true,
 });
+
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    return config;
+  });
+} // O interceptors vai interceptar cada requisição, e essa função recebe a config, config são os dados das requisições o corpo o cabeçalho, no caso quero somente colocar um delay nelas.
